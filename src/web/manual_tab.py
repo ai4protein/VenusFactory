2154524f208052f6a80e38f2d1b4008d1871b1e8
Wilson Foo <h1467943075@gmail.com>
2025-09-30 04:12:38 +0800

    Update manual_tab.py

--- src/web/manual_tab.py
+++ src/web/manual_tab.py
@@ -454,14 +454,14 @@ def update_manual(language):
     """
     
     return  venusscope_output,      # 1
-            venusagent_output,      # 2
-            training_output,        # 3
-            prediction_output,      # 4
-            evaluation_output,      # 5
-            quicktools_output,      # 6
-            advancedtools_output,   # 7
-            download_output,        # 8
-            faq_output              # 9
+    venusagent_output,      # 2
+    training_output,        # 3
+    prediction_output,      # 4
+    evaluation_output,      # 5
+    quicktools_output,      # 6
+    advancedtools_output,   # 7
+    download_output,        # 8
+    faq_output              # 9
 
 def load_manual_training(language):
     if language == 'Chinese':
